9fa200fca050eb2d80a1a23be9786877d8049a88
Tanmoy Joy <dukejay97@gmail.com>
2022-03-01 19:53:40 +0600

    added assetPrefix basepath and image url

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,38 +1,54 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
+
+const prefix = "/portfolio";
 
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {
-        projects?.map((project)=> (
-          <BlogCard key={project.id}>
-            <Img src={project.image} />
-            <TitleContent>
-              <HeaderThree title>{project.title}</HeaderThree>
-              <Hr />
-            </TitleContent>
-            <CardInfo>{project.description}</CardInfo>
-            <div>
-              <TitleContent>Stack</TitleContent>
-              <TagList>
-                {project.tags.map((tag, i)=> (
-                  <Tag key={i}>{tag}</Tag>
-                ))}
-              </TagList>
-            </div>
-            <UtilityList>
-              <ExternalLinks href={project.visit}>Code</ExternalLinks>
-              <ExternalLinks href={project.source}>Live Demo</ExternalLinks>
-            </UtilityList>
-          </BlogCard>
-        ))
-      }
+      {projects?.map((project) => (
+        <BlogCard key={project.id}>
+          <Img src={prefix + project.image} />
+          <TitleContent>
+            <HeaderThree title>{project.title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{project.description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {project.tags.map((tag, i) => (
+                <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={project.visit}>Code</ExternalLinks>
+            <ExternalLinks href={project.source}>Live Demo</ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+      ))}
     </GridContainer>
   </Section>
 );
